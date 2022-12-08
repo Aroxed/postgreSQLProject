96e32e8feb0ad24f03cc8154d6e679750b7b3ad5
Andrii Petrashenko <petrashenko@gmail.com>
2022-12-08 15:34:36 +0200

    Initial

---
+++ main_db.py
@@ -0,0 +1,10 @@
+from db_class import Database
+
+database = Database()
+database.connect()
+database.insert_initial_data()
+database.print()
+database.delete(person_id=2)
+database.print()
+database.delete_all()
+database.print()
